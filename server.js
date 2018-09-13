7399c27b099c952986dde9d9c49ed58ffbf2e3f2
ge-hall <Andrew.Ge-Hall@healthcarelogic.com.au>
2018-09-13 20:48:30 +1000

    Updated editorconfig to be consistent with linting modified lint run no-console to warn

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
-require('dotenv').config();
-const feathers = require('@feathersjs/feathers');
-
-var knex = require('knex')({
-  client: 'pg',
-  version: '7.2',
-  connection: {
-    host : '127.0.0.1',
-    user : process.env.DB_USER,
-    password : 'process.env.DB_PASS',
-    database : 'process.env.DB_NAME'
-  }
-});
+require("dotenv").config();
+const feathers = require("@feathersjs/feathers");
 const app = feathers();
+var knex = require("knex")({
+    client: "pg",
+    version: "7.2",
+    connection: {
+        host: "127.0.0.1",
+        user: process.env.DB_USER,
+        password: "process.env.DB_PASS",
+        database: "process.env.DB_NAME"
+    }
+});
+
+// Register a simple todo service that returns the name and some text
+app.use("todos", {
+    async get(name) {
+        // Return an object in the form of { name, text }
+        return {
+            name,
+            text: `You have to do ${name}`
+        };
+    }
+});
+
+/**
+ * gets a list of todo items
+ *
+ * @param {*} name - name of todo item
+ */
+async function getTodo(name) {
+    // Get the service we registered above
+    const service = app.service("todos");
+    // Call the `get` method with a name
+    const todo = await service.get(name);
+
+    // Log the todo we got back
+    console.log(todo);
+}
+
+getTodo("dishes");
